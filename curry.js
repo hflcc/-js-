85d89b0eb96a2df8d20b4cbda8345ea4f91a6f78
DESKTOP-79SAAVH\DELL <hingbelieve@163.com>
2021-06-07 20:54:59 +0800

    update curry

---
+++ curry.js
@@ -0,0 +1,18 @@
+// todo 待优化
+
+function curry(fn, len = fn.length) {
+    return _curry.call(this, fn, len)
+}
+
+function _curry(fn, len, ...args) {
+    return function (...params) {
+        let _args = [...args, ...params]
+        if (_args.length >= len) {
+            return fn.apply(this, _args)
+        } else {
+            return _curry.call(this, fn, len, ...args)
+        }
+    }
+}
+
+export default curry
